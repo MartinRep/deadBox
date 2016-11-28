2d91addec2f8ec0a71eeb7c1eb3e41bfb5eff077
repickym <repickym@gmail.com>
2016-11-28 16:24:43 +0000

    PostgresSQL setup and DB init

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:maximus14@localhost/deadBox' #postgresql://username(db):password@urlAddress/DBname
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(10), unique=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(50))
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 @app.route('/')
 def index():
@@ -8,4 +25,5 @@ def index():
 
 
 if __name__ == "__main__":  #make sure the app will be run on it's own, not as API
+    
     app.run()
